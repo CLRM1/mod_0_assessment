f7319bbc1327d6caba6c7bde7d8d4b497bf1dd34
Chris Romero <chrisleeromero1@gmail.com>
2021-12-16 13:27:26 -0700

    Add software developer variable assigned to hash

---
+++ models/model_b.rb
@@ -0,0 +1,13 @@
+new_software_developer = {
+  first_name: "Chris",
+  last_name: "Romero",
+  favorite_programming_language: "Ruby",
+  age: 33,
+  number_of_pets: 1,
+  height_in_inches: 68.5,
+  turing_graduate: false,
+  currently_working: false,
+  siblings: ["Michael", "Jason"],
+  languages_spoken: ["English", "Spanish"],
+  favorite_numbers: [1, 2, 4, 8, 16,]
+}
