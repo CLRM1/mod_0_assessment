43b32b61f32f26572799bf4c6f22f57fa7d91856
Chris Romero <chrisleeromero1@gmail.com>
2021-12-16 13:05:26 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+movie = "Office Space"
+
+is_entertaining = true
+
+movie = "Star Trek"
+
+num_4 = 20
+
+num_6 = 30
+
+product = num_6 * num_4
+
+favorite_foods = ["pizza", "tacos", "chips and salsa"]
+
+favorite_foods.delete_at(0)
+
+puts movie.length
