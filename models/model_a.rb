238f56940a48869f1ca5e7ce0af40738d7b7fe9c
Chris Romero <chrisleeromero1@gmail.com>
2021-12-16 13:17:58 -0700

    Add tyrannosaurus variable data model with hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+tyrannosaurus_rex = {
+  name: "Jerry",
+  size: "large",
+  teeth: 50,
+  speed: 25.5,
+  is_dangerous: true,
+  able_to_fly: false,
+  diet: ["mammals", "small carnivores", "herbivores"],
+  locations: ["North America", "South America", "Europe"]
+}
